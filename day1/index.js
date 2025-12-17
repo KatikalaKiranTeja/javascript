3c3a22e1e1b09a370744c5d7074bbd4fc1f64e44
Kiran <kirantejakatikala64@gmail.com>
2025-12-17 09:47:14 +0530

    js

---
+++ day1/index.js
@@ -0,0 +1,30 @@
+// console.log("hello.....")
+
+
+// let a;
+// a=10
+// // a=11
+// console.log(a)
+
+// var b;
+// b="pulihora"
+// // b="biryani"
+// console.log(b)
+
+// const c="hello";
+// console.log(c)
+
+
+        function scope(){
+    // let a=10
+    console.log(a)
+    let a=20
+    console.log(a)
+  
+    var b=20
+    // console.log(a)
+    console.log(b)
+}
+scope()
+// let a=10
+// console.log(a)
